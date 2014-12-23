a8f2dbb4f9083ff6da7f6e6194b09823a716cc0b
Otter <otc@otterthecat.com>
2014-12-22 20:57:06 -0600

    added exaggerated y axis world bounds. Blocks are "destroyed" on impact

--- hello-phaser/car-vertical.js
+++ hello-phaser/car-vertical.js
@@ -18,7 +18,7 @@
             game.physics.startSystem(Phaser.Physics.AUTO);
             // when adding tileSprite, we're positioning tiles
             // so as to create a 50px border around the game world
-            terrain = game.add.tileSprite(50, 0, 700, 600, 'terrain');
+            terrain = game.add.tileSprite(50, 0, 700, 3600, 'terrain');
 
 
             blocks = game.add.group();
@@ -33,14 +33,14 @@
 
             function launchBlock() {
                 var MIN_BLOCK_SPACING = 300;
-                var MAX_BLOCK_SPACING = 1800;
-                var BLOCK_SPEED = -300;
+                var MAX_BLOCK_SPACING = 3600;
+                var BLOCK_SPEED = -200;
 
                 var block = blocks.getFirstExists(false);
                 if (block) {
-                    block.reset(game.rnd.integerInRange(0, game.width), 728);
+                    block.reset(game.rnd.integerInRange(0, game.width), 3728);
                     block.body.velocity.y = BLOCK_SPEED;
-                    block.body.drag.x = 100;
+                    block.body.bounce.set(0.8);
                 }
                 game.time.events.add(game.rnd.integerInRange(MIN_BLOCK_SPACING, MAX_BLOCK_SPACING), launchBlock);
             }
@@ -52,16 +52,22 @@
 
             game.physics.enable(car, Phaser.Physics.ARCADE);
             car.body.collideWorldBounds = true;
-            car.body.bounce.set(0);
+            car.body.bounce.set(0.1);
 
             cursors = game.input.keyboard.createCursorKeys();
 
-            game.world.setBounds(0, 0, 800, 600);
+            game.camera.follow(car);
+            game.world.setBounds(0, 0, 800, 3600);
+            game.world.position.y = -3200;
+        }
+
+        function killBlock (player, enemy) {
+            enemy.kill();
         }
 
         function update () {
 
-            game.physics.arcade.collide(car, blocks);
+            game.physics.arcade.collide(car, blocks, killBlock, null, this);
 
             if (cursors.left.isDown) {
                 car.body.velocity.x = -200;
@@ -71,15 +77,15 @@
             }
 
             if (cursors.up.isDown) {
-                terrain.tilePosition.y += 5;
+                car.body.velocity.y = -220;
             }
             else if (cursors.down.isDown) {
-                terrain.tilePosition.y -= 5;
+                car.body.velocity.y = 200;
             }
         }
 
         function render () {
             // uncomment to view debug bounding box
-            game.debug.body(car);
+            // game.debug.body(car);
         }
     };
